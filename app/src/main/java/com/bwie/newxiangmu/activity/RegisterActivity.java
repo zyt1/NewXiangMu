dca5a351ab0260d1fe77fd6633394f8231c66435
jiachengbing <3480140587@qq.com>
2017-12-20 08:57:40 +0800

    //贾成兵第一次修改

--- app/src/main/java/com/bwie/newxiangmu/activity/RegisterActivity.java
+++ app/src/main/java/com/bwie/newxiangmu/activity/RegisterActivity.java
@@ -11,5 +11,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //亚涛牙套
     }
 }
